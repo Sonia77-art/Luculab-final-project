62ec82897aafe6fc27712c72974d52f195c706ff
Sonia77-art <soniasobby@gmail.com>
2022-11-19 01:38:33 +0200

    test

--- luculab-final-project/src/Components/Words.jsx
+++ luculab-final-project/src/Components/Words.jsx
@@ -1,21 +1,56 @@
 import "../App.css";
+import { useState } from "react";
 
-export default function Words() {
-  function handleInformationClick(event) {}
-  const words = ["son", "box", "hide", "dress", "bed"];
+//* export default function Words() {
+// function handleInformationClick(event) {}
+// const words = ["son", "box", "hide", "dress", "bed"];
+
+// return (
+// <div className="Wds">
+// {words.map((word) => (
+// <button
+// className="btn-2"
+// onClick={() => handleInformationClick()}
+// style={{ backgroundColor: "#42C2FF" }}
+// key={word}
+// >
+// {word}
+// </button>
+// ))}
+// </div>
+// );
+// }
+
+export default function Button(props) {
+  const [color, setColors] = useState("blue");
+  const [active, setActive] = useState(false);
+  const handleClickButton = (words) => {
+    setActive(true);
+    setColors(words);
+    if (active === true) {
+      setActive(false);
+      setColors("blue");
+    }
+  };
 
   return (
-    <div className="Wds">
+    <button
+      className={`button ${color}`}
+      onClick={() => handleClickButton(props.words)}
+    >
+      {props.words}
+    </button>
+  );
+}
+
+const Words = () => {
+  const [words, setWords] = useState(["son", "bed", "box", "hide", "dress"]);
+
+  return (
+    <div className="container">
       {words.map((word) => (
-        <button
-          className="btn-2"
-          onClick={() => handleInformationClick()}
-          style={{ backgroundColor: "#42C2FF" }}
-          key={word}
-        >
-          {word}
-        </button>
+        <Button key={word} word={word} />
       ))}
     </div>
   );
-}
+};
