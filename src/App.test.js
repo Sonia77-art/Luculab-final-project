2fee2980b8c4073e819218d4b31fe950e113d875
Sonia77-art <soniasobby@gmail.com>
2022-11-29 15:02:23 +0200

    test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Excerise-1/i);
+  const linkElement = screen.getByText(/Excerise-3/i);
   expect(linkElement).toBeInTheDocument();
 });
